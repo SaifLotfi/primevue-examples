be3cf5cde33cbd9ee1646fb7ff6669c508cdf119
saif <saiflotfi@proton.me>
2024-11-07 10:00:14 +0200

    add auto imports

--- vite-ts-quickstart/src/main.ts
+++ vite-ts-quickstart/src/main.ts
@@ -1,24 +1,27 @@
-import { createApp } from "vue";
-import PrimeVue from "primevue/config";
-import Aura from "@primevue/themes/aura";
-import Button from "primevue/button";
-import Toast from "primevue/toast";
-import ToastService from "primevue/toastservice";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import ToastService from 'primevue/toastservice';
+import App from './App.vue';
 
-import "./assets/main.css";
-import "primeicons/primeicons.css";
+import './assets/main.css';
+
+import 'primeicons/primeicons.css';
 
 const app = createApp(App);
 
 app.use(PrimeVue, {
-    theme: {
-        preset: Aura,
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false,
     },
+  },
+  ripple:true
 });
+
 app.use(ToastService);
 
-app.component("Button", Button);
-app.component("Toast", Toast);
-
-app.mount("#app");
+app.mount('#app');
--- vite-ts-quickstart/vite.config.ts
+++ vite-ts-quickstart/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
+export default defineConfig(async () => {
+  const { PrimeVueResolver } = await import('@primevue/auto-import-resolver');
+  return {
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [PrimeVueResolver()],
+      }),
+    ],
+  };
 });
